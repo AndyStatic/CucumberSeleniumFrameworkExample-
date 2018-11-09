f2e387a80ab0a565608b89753f03da13b1122d75
kalasnikovs.andrejs <a.kalasnikovs@reply.com>
2018-11-09 15:27:02 +0000

    Google Search page and locator complete

--- src/test/java/Locators/GoogleSearchPageLocators.java
+++ src/test/java/Locators/GoogleSearchPageLocators.java
@@ -3,5 +3,6 @@ package Locators;
 public class GoogleSearchPageLocators {
 
     public static final String GOOGLE_SEACH_PAGE = "https://www.google.com";
-
+    public static final String SEARCH_BOX_ID_LOCATOR = "lst-ib";
+    public static final String SEARCH_BUTTON_XPATH_LOCATOR = "//input[@value='Google Search' and @type='submit']";
 }
--- src/test/java/Steps/GoogleSearchPage.java
+++ src/test/java/Steps/GoogleSearchPage.java
@@ -5,6 +5,7 @@ import Steps.Base.BaseUtil;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 
 public class GoogleSearchPage extends BaseUtil {
 
@@ -24,11 +25,12 @@ public class GoogleSearchPage extends BaseUtil {
 
     @When("^I enter the search keyword \"([^\"]*)\"$")
     public void iEnterTheSearchKeyword(String keyword) throws Throwable {
-        System.out.println("I enter the search keyword");
+        base.driver.findElement(By.id(googleSearchPageLocators.SEARCH_BOX_ID_LOCATOR)).sendKeys(keyword);
     }
 
     @And("^I press the \"([^\"]*)\" button$")
     public void iPressTheButton(String button) throws Throwable {
-        System.out.println("I press the");
+        if (button.equals("Google Search"))
+            base.driver.findElement(By.xpath(googleSearchPageLocators.SEARCH_BUTTON_XPATH_LOCATOR)).click();
     }
 }
