4aa4c64b4648f61e341c2ee67c9e48d22bdc02c8
kalasnikovs.andrejs <a.kalasnikovs@reply.com>
2018-11-09 18:56:31 +0000

    html file report generation

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -3,6 +3,6 @@ package Runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = {"src/test/java/Features"}, format = {"json:target/cucumber.json"}, glue = "Steps")
+@CucumberOptions(features = {"src/test/java/Features"}, format = {"json:target/cucumber.json", "html:target/cucumber-html"}, glue = "Steps")
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
